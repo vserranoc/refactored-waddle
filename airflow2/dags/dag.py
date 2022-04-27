2f8efcb72bb98c2d3b2eadbfc0b3db3aa1f2a50d
pautrejo <paulinahtrejo@gmail.com>
2022-04-27 09:39:42 -0500

    agrego dag etl primera parte

--- airflow2/dags/dag.py
+++ airflow2/dags/dag.py
@@ -7,15 +7,12 @@ from airflow.contrib.operators.bigquery_operator import BigQueryOperator
 from airflow.models import Variable
 from datetime import datetime
 from airflow.utils.dates import days_ago
-from tweets import get_write_tweets
+from tweets import *
 import os
 
 
 params = {
-    'access_token': Variable.get("access_token"),
-    'access_token_secret': Variable.get("access_token_secret"),
-    'consumer_key': Variable.get("consumer_key"),
-    'consumer_secret': Variable.get("consumer_secret"),
+    'bearer': Variable.get("bearer"),
     'project_id': Variable.get("project_id")
     }
 
@@ -31,46 +28,37 @@ args = {
 dag = DAG(
     dag_id='extraction',
     default_args=args,
-    schedule_interval='0 0 * * *',
+    schedule_interval='*/2 * * * *',
     start_date=days_ago(2),
     tags=['bash', 'python', 'tweet', 'extraction'],
     max_active_runs=1
 )
 
 
-task1 = BashOperator(
+task1 = PythonOperator(
     dag=dag,
-    task_id='task1',
-    bash_command='pip install tweepy'
-)
-
-task2 = PythonOperator(
-    dag=dag,
-    task_id = 'task2',
+    task_id = 'task1',
     provide_context=True,
     python_callable=get_write_tweets,
-    op_kwargs={'access_token': params['access_token'], 'access_token_secret': params['access_token_secret'],
-    'consumer_key': params['consumer_key'],'consumer_secret': params['consumer_secret'],
-    'project_id': params['project_id'],
-    'database': 'tweets',
+    op_kwargs={'bearer': params['bearer'],'project_id': params['project_id'],
+    'database': 'tweets_extraction',
     'table': 'JetBlue',
     'search': 'JetBlue'
     }
 )
 
-task3 = PythonOperator(
+task2= PythonOperator(
     dag=dag,
-    task_id = 'task3',
+    task_id = 'task2',
     provide_context=True,
     python_callable=get_write_tweets,
-    op_kwargs={'access_token': params['access_token'], 'access_token_secret': params['access_token_secret'],
-    'consumer_key': params['consumer_key'],'consumer_secret': params['consumer_secret'],
-    'project_id': params['project_id'],
-    'database':'tweets',
+    op_kwargs={'bearer': params['bearer'],'project_id': params['project_id'],
+    'database':'tweets_extraction',
     'table': 'SouthwestAir',
     'search': 'SouthwestAir'
     }
 )
 
 
-task1 >> [task2,task3]
+task1
+task2
