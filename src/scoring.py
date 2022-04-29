49daaf339a57cac54104b9f97431d96b4b90b4f4
jesusmb230795 <jesusmiranda2307@gmail.com>
2022-04-29 18:07:36 -0500

    update model objects a scoring script

--- src/scoring.py
+++ src/scoring.py
@@ -3,13 +3,13 @@ from src.clean_string import clean_tweet
 
 # Load
 final_model = joblib.load('./src/objects/final_model.joblib')
-count_vec = joblib.load('./src/objects/model_count_vectorizer.joblib')
+best_preprocessing = joblib.load('./src/objects/feature_extractor.joblib')
 
 
 def predict_sentiment(text):
     clean_text = [clean_tweet(text)]
     print(clean_text)
-    transform_text = count_vec.transform(clean_text) 
+    transform_text = best_preprocessing.transform(clean_text) 
     proba = final_model.predict_proba(transform_text)
     
     return f'Probability of being a positive sentiment: {proba[0,-1]}'
