b93901c3c1c35dadf59937a6601d2a39bb5cfe04
jesusmb230795 <jesusmiranda2307@gmail.com>
2022-05-01 22:47:30 -0500

    update scoring function

--- src/scoring.py
+++ src/scoring.py
@@ -2,11 +2,18 @@ import joblib
 from src.clean_string import clean_tweet   
 
 # Load
-final_model = joblib.load('./src/objects/final_model.joblib')
-best_preprocessing = joblib.load('./src/objects/feature_extractor.joblib')
+final_model = joblib.load('./src/artifacts/final_model.joblib')
+best_preprocessing = joblib.load('./src/artifacts/feature_extractor.joblib')
 
 
 def predict_sentiment(text):
+    clean_text = [clean_tweet(text)]
+    transform_text = best_preprocessing.transform(clean_text) 
+    proba = final_model.predict_proba(transform_text)
+    
+    return proba[0,-1]
+
+def proof_sentiment_predicted(text):
     clean_text = [clean_tweet(text)]
     print(clean_text)
     transform_text = best_preprocessing.transform(clean_text) 
